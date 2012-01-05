72d238ca2f83908a131927b53b88a0cfaf2f526b
Andreas Brisner <andreas@brisner.no>
2012-01-06 00:20:16 +0100

    Adding MySQL again

---
+++ src/no/brisner/minetime/Donations.java
@@ -0,0 +1,22 @@
+package no.brisner.minetime;
+
+import lib.PatPeter.SQLibrary.*;
+
+import no.brisner.minetime.Minetime;
+import no.brisner.minetime.Settings;
+
+public class Donations {
+	
+	public void getDonations(String player) {
+		
+	}
+	public void addDonations(String player) {
+		
+	}
+	public void getTokens(String player) {
+		
+	}
+	public void addTokens(String player) {
+		
+	}
+}
